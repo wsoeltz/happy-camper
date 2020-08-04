b5af1e726a90f3e74fc2b090b7cce810a7317c43
Kyle Soeltz <wsoeltz@gmail.com>
2020-08-04 17:12:51 -0400

    Remove whitenoise

--- happy_camper_1/settings.py
+++ happy_camper_1/settings.py
@@ -150,19 +150,3 @@ JWT_AUTH = {
 DEBUG=True
 
 CORS_ORIGIN_ALLOW_ALL = DEBUG
-
-
-# Static assets config for Heroku
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
